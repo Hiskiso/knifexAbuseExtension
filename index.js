46f8611aeb730588a7fddc2e1926094338c5d7a6
Roman <roma123qq@gmail.com>
2022-01-02 22:03:23 +0400

    add ui

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 window.accounts = [];
 window.captcha = [];
 window.timers = []
-
+window.logs = ""
+window.loger = (message, style="")=>{console.log(message, style); window.logs = window.logs + message + "\n"}
 
 window.onload = () => {
 
@@ -12,16 +13,121 @@ window.onload = () => {
         if (result.account) {
             window.accounts = [result.account];
         } else {
-            console.log("%cACCOUNT NOT ENTERED", "color: red; font-size: 50px")
+            window.loger("%cGet account by cookie", "color: blue; font-size: 50px");
+            let id = parseCookie(document.cookie).id
+            chrome.storage.sync.set({account: id}, ()=>{})
         }
 
     });
+
+    setInterval(()=>{
+      try {
+        document.title = document.querySelector("#accordion__panel-giveaway > div.chat-giveaway__note").innerText + " | " + document.querySelector("#accordion__heading-giveaway > span:nth-child(3)").innerText
+      } catch (error) {
+          document.title = document.querySelector("#accordion__heading-giveaway > span:nth-child(2)").innerText
+      }
+       
+    }, 1000)
+
 }
 
-function knifexAlert(message, type="good"){
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        switch (request.command) {
+            case "getlogs":
+                {
+                    sendResponse(window.logs)
+                }
+                break;
+        
+            default:
+                break;
+        }
+    });
+
+function websoketMain() {
+    websk = new WebSocket("wss://knifex.best:2083/socket.io/?EIO=3&transport=websocket");
+
+    websk.onopen = () => {
+        window.loger(window.accounts);
+        websk.send(`420["join",{"ott":"9306472d-949b-4258-99b4-774752860cd1"}]`)
+        window.loger("%c>   " + 'Joined', "color: blue");
+        websk.send(2)
+        setInterval(() => {
+            window.loger(">   " + '2');
+            websk.send(2)
+        }, 25000);
+    };
+
+    websk.onmessage = (data) => {
+
+
+
+        window.loger('<   ' + data.data);
+
+        if (data.data.toString().substring(0, 2) == '42') {
+
+            data = JSON.parse(data.data.toString().substring(2))
+            const promo = data[1]?.newPromoQuery?.name;
+            window.loger("%c" + promo, "color: green; background: #0f2c51; font-size: 30px");
+
+
+
+            window.accounts.forEach(async (account, num) => {
+                fetch(`https:///knifex.best/api/user/freebie/promo`, {
+                    method: 'POST',
+                    body: JSON.stringify({ exclusive: false, promocode: promo, "captcha": window.captcha[num] }),
+                    headers: {
+                        "content-type": "application/json",
+                        "meta-data": account,
+                        "cookie": `id=${account}`,
+                    }
+                }).then(r => r.json()).then(r => {
+                    if (r.data == "LIMIT") {
+                        clearTimers();
+                        knifexAlert("Pleace update captcha", "bad")
+                        window.loger("%cLIMIT", "color: red; background: #0f2c51; font-size: 30px");
+                    }
+                    else if (r.ok == true) {
+                        knifexAlert("PROMO ACTIVATED");
+                        window.loger("%cPROMO +++ " + account + " " + JSON.stringify(r) + " ", "color: green; background: #0f2c51; font-size: 30px")
+                    }
+                    else {
+                        window.loger(JSON.stringify(r) + ' ' + account)
+                    }
+                })
+            });
+            clearTimers()
+
+        }
+    }
+
+    websk.onclose = () => {
+        websoketMain()
+    }
+
+}
+
+
+function clearTimers() {
+    window.timers.map(el => { clearTimeout(el); window.loger("All timers clear") })
+
+}
+
+const parseCookie = str =>
+    str
+        .split(';')
+        .map(v => v.split('=')) 
+        .reduce((acc, v) => {
+            acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+            return acc;
+        }, {});
+
+
+function knifexAlert(message, type = "good") {
     let elAlertKnifex = document.getElementById("__react-alert__");
-   elAlertKnifex.innerHTML = `<div loginerrtimeout="7000" style="left: 0px; position: fixed; display: flex; justify-content: center; align-items: flex-start; flex-direction: column; width: 100%; pointer-events: none; bottom: 0px; z-index: 100;"><div style="transform: scale(1); transition: all 250ms ease-in-out 0s;"><div class="ntf ntf--${type=="good"? "good": "bad"}" style="margin: 10px; pointer-events: all;">${message}</div></div></div>`
-    setTimeout(()=>{elAlertKnifex.innerHTML = ""}, 7000)
+    elAlertKnifex.innerHTML = `<div loginerrtimeout="7000" style="left: 0px; position: fixed; display: flex; justify-content: center; align-items: flex-start; flex-direction: column; width: 100%; pointer-events: none; bottom: 0px; z-index: 100;"><div style="transform: scale(1); transition: all 250ms ease-in-out 0s;"><div class="ntf ntf--${type == "good" ? "good" : "bad"}" style="margin: 10px; pointer-events: all;">${message}</div></div></div>`
+    setTimeout(() => { elAlertKnifex.innerHTML = "" }, 7000)
 }
 
 function captchaUpdater() {
@@ -34,75 +140,16 @@ function captchaUpdater() {
             knifexAlert("Captcha added")
             window.temp = document.querySelector("#g-recaptcha-response").value
             window.captcha.shift()
-            console.log("%cCaptcha added", "color: green")
+            window.loger("%cCaptcha added", "color: green")
             window.captcha.push(window.temp);
             let ifr = document.querySelector("#root > div > div.AppLayout > div > div.bonus-col.gbonus > div.abonus.layoutSection > div.abonus-container.abonus-container-main > form > div.abonus-container__recaptcha > div > div > div > div > div > iframe")
             ifr.src = ifr.src
 
-            let timer = setTimeout(() => {knifexAlert("Captcha Expired", "bad"); console.log("%cCaptcha Expired!!", "color: red; background: #0f2c51; font-size: 30px"); }, 60000)
+            let timer = setTimeout(() => { knifexAlert("Captcha Expired", "bad"); window.loger("%cCaptcha Expired!!", "color: red; background: #0f2c51; font-size: 30px"); }, 60000)
             window.timers.push(timer)
 
         }
     }, 1000)
 
-
-}
-
-function clearTimers() {
-    window.timers.map(el => { clearTimeout(el); console.log("All timers clear") })
-
-}
-
-function websoketMain() {
-    websk = new WebSocket("wss://knifex.best:2083/socket.io/?EIO=3&transport=websocket");
-
-    websk.onopen = () => {
-        console.log(window.accounts);
-        websk.send(`420["join",{"ott":"9306472d-949b-4258-99b4-774752860cd1"}]`)
-        console.log("%c>   " + 'Joined', "color: blue");
-        websk.send(2)
-        setInterval(() => {
-            console.log(">   " + '2');
-            websk.send(2)
-        }, 25000);
-    };
-
-    websk.onmessage = (data) => {
-
-        
-
-        console.log('<   ' + data.data);
-
-        if (data.data.toString().substring(0, 2) == '42') {
-
-            data = JSON.parse(data.data.toString().substring(2))
-            const promo = data[1]?.newPromoQuery?.name;
-            console.log("%c" + promo, "color: green; background: #0f2c51; font-size: 30px");
-
-            if (data.data == "LIMIT") {
-                clearTimers();
-                knifexAlert("Pleace update captcha", "bad")
-                console.log("%cLIMIT", "color: red; background: #0f2c51; font-size: 30px");
-            } else {
-
-                window.accounts.forEach(async (account, num) => {
-                    fetch(`https:///knifex.best/api/user/freebie/promo`, {
-                        method: 'POST',
-                        body: JSON.stringify({ exclusive: false, promocode: promo, "captcha": window.captcha[num] }),
-                        headers: {
-                            "content-type": "application/json",
-                            "meta-data": account,
-                            "cookie": `id=${account}`,
-                        }
-                    }).then(r => r.json()).then(r => { if (r.ok == true) { knifexAlert("PROMO ACTIVATED");console.log("%cPROMO +++ " + account + " " + JSON.stringify(r) + " ", "color: green; background: #0f2c51; font-size: 30px") } else { console.log(JSON.stringify(r) + ' ' + account) } })
-                });
-                clearTimers()
-            }
-        }
-    }
-
-    websk.onclose = () => {
-        websoketMain()
-    }
 
 }
--- public/options.js
+++ public/options.js
@@ -1,18 +1,46 @@
 document.getElementById("form").addEventListener("submit", addnew)
-let id = document.getElementById("idAcc")
+let note = document.querySelector("body > div > div.abonus-container__note")
+let input = document.querySelector("#new_account")
+input.onclick = function () {
+    this.select();
+   }
 
 function addnew(e){
 e.preventDefault();
-chrome.storage.sync.set({account: e.target.children[0].value}, ()=>{})
-updateList()
+if (e.target.children[0].children[0].children[0].value.charAt(0) == "/") {
+    let command = e.target.children[0].children[0].children[0].value.slice(1)
+    switch (command) {
+        case "getlogs":
+            {
+                chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+                    var activeTab = tabs[0];
+                    chrome.tabs.sendMessage(activeTab.id, {"command": command}, (response)=>{navigator.clipboard.writeText(response)});
+                   });
+                   e.target.children[0].children[0].children[0].value = "";
+                   note.innerText = "Скопированно в буфер обмена"
+                   updateList()
+            }
+            break;
+    
+        default:
+            {
+                e.target.children[0].children[0].children[0].value = "";
+                input.placeholder = "Комманда не найдена"
+            }
+            break;
+
+    }
+} else{
+    chrome.storage.sync.set({account: e.target.children[0].value}, ()=>{})
+    updateList()
+}
+
 }
 
 function updateList(){
     chrome.storage.sync.get(['account'], function(result) {
         if (result.account) {
-            id.innerText = result.account;
-        } else{
-            d.innerText = "Введите ниже айди"
+            input.value = result.account;
         }
           
        });
