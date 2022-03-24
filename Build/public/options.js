c572f80afed9fc71e14e113a557c3e1fcc969b42
Roman <roma123qq@gmail.com>
2022-03-24 19:45:24 +0400

    build

---
+++ Build/public/options.js
@@ -0,0 +1,43 @@
+document.getElementById("form").addEventListener("submit", addnew)
+let infonote = document.querySelector("body > div > div.abonus-container__note")
+let input = document.querySelector("#new_account")
+let versionNoteEl = document.querySelector(".version_note")
+let manifestData = chrome.runtime.getManifest();
+
+function addnew(e) {
+    e.preventDefault();
+
+    chrome.storage.sync.set({ account: e.target.children[0].children[0].children[0].value }, () => { })
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+
+        chrome.tabs.sendMessage(activeTab.id, { "command": "reload" }, (response) => { });
+
+
+    });
+
+
+
+}
+
+function updateList() {
+    chrome.storage.sync.get(['account'], function (result) {
+        if (result.account) {
+            input.value = result.account;
+        }
+
+    });
+
+}
+
+
+window.onload = () => {
+    versionNoteEl.innerText = "v " + manifestData.version
+    input.onclick = function () {
+        this.select();
+    }
+    updateList()
+
+}
+
